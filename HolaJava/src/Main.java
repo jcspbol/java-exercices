d5346768bc1ee494b5ff4d76e81428d704083d09
Juan Carlos Serna <jcspbol@gmail.com>
2024-06-10 17:00:16 -0400

    se aunmento comentario de tecnologia

--- HolaJava/src/Main.java
+++ HolaJava/src/Main.java
@@ -11,7 +11,7 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
             System.out.println("Hola Juan Carlos Serna");
-
+            System.out.println("La programacion es el arte para la tecnologia");
         }
     }
 }
